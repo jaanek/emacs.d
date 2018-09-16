c0852ccb7d0cee4d1febd16f6e658e6120dd5165
Jaanek Oja <jaanek@oja.me>
2018-09-16 13:03:57 +0300

    removed unneccessary configuration

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -3,15 +3,21 @@
 ;; (global-set-key [f8] 'neotree-toggle)
 
 (require-package 'go-mode)
-(require-package 'company-go)
+(require-package 'company)
+(require-package 'company-go) ;; autocomplete - https://github.com/mdempsky/gocode/tree/master/emacs-company
+
+;; go-code setup
+;; (require 'go-autocomplete)
+;; (require 'auto-complete-config)
+;; (ac-config-default)
+
+;;(require-package 'go-guru)
+;;(require-package 'go-rename)
+
 (add-hook 'before-save-hook 'gofmt-before-save)
 
-(require 'go-autocomplete)
-(require 'auto-complete-config)
-(ac-config-default)
-
-(add-to-list 'load-path "~/go/src/github.com/dougm/goflymake")(require 'go-flymake)
-(add-to-list 'load-path "~/go/src/github.com/dougm/goflymake")(require 'go-flycheck)
+;; (add-to-list 'load-path "~/go/src/github.com/dougm/goflymake")(require 'go-flymake)
+;; (add-to-list 'load-path "~/go/src/github.com/dougm/goflymake")(require 'go-flycheck)
 
 (add-hook 'go-mode-hook 'company-mode)(add-hook 'go-mode-hook (lambda ()  (set (make-local-variable 'company-backends) '(company-go)) (company-mode)))
 
@@ -28,7 +34,6 @@
 ;; Define function to call when go-mode loads
 ;; https://johnsogg.github.io/emacs-golang
 (defun my-go-mode-hook ()
-  (add-hook 'before-save-hook 'gofmt-before-save) ; gofmt before every save
   ;; Key bindings specific to go-mode
   (local-set-key (kbd "M-.") 'godef-jump) ; Go to definition
   (local-set-key (kbd "C-c C-]") 'next-error) ; Go to next error (or msg)
@@ -37,8 +42,5 @@
 
 ;; Connect go-mode-hook with the function we just defined
 (add-hook 'go-mode-hook 'my-go-mode-hook)
-
-;; If the go-guru.el file is in the load path, this will load it.
-(require 'go-guru)
 
 (provide 'init-go)
