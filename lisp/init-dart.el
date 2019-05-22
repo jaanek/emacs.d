cd67bc8401595724cd71fabb2f4048e9f071df91
Jaanek Oja <jaanek@oja.me>
2019-05-22 13:40:20 +0300

    dart mode

--- lisp/init-dart.el
+++ lisp/init-dart.el
@@ -1,9 +1,16 @@
-(require-package 'dart-mode)
 ;; https://github.com/bradyt/dart-mode
+(require-package 'dart-mode)
+(require-package 'lsp-mode)
+;; (require-package 'lsp-ui)
 
-(setq dart-enable-analysis-server t)
+;; (setq dart-enable-analysis-server t)
 (setq dart-format-on-save t)
+;; (defcustom lsp-dart-sdk-dir "~/flutter/bin/cache/dart-sdk/" by default in lsp-mode
+(setq lsp-dart-sdk-dir "/opt/flutter/bin/cache/dart-sdk/")
 
+;; https://github.com/emacs-lsp/lsp-mode
+;; (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+;; (add-hook 'dart-mode-hook #'lsp)
 (add-hook 'dart-mode-hook 'flycheck-mode)
 
 (provide 'init-dart)
