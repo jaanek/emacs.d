546455203daedd00c1ba470cc52992ade9ebea66
Jaanek Oja <jaanek@oja.me>
2020-04-04 10:50:24 +0300

    dart mode

--- lisp/init-dart.el
+++ lisp/init-dart.el
@@ -3,14 +3,18 @@
 (require-package 'lsp-mode)
 ;; (require-package 'lsp-ui)
 
-;; (setq dart-enable-analysis-server t)
-(setq dart-format-on-save t)
-;; (defcustom lsp-dart-sdk-dir "~/flutter/bin/cache/dart-sdk/" by default in lsp-mode
 (setq lsp-dart-sdk-dir "/opt/flutter/bin/cache/dart-sdk/")
+(setq dart-format-on-save t)
 
 ;; https://github.com/emacs-lsp/lsp-mode
 ;; (add-hook 'lsp-mode-hook 'lsp-ui-mode)
-;; (add-hook 'dart-mode-hook #'lsp)
-(add-hook 'dart-mode-hook 'flycheck-mode)
+
+;; lsp
+;; https://github.com/bradyt/dart-mode/wiki/LSP
+(add-hook 'dart-mode-hook 'lsp)
+(with-eval-after-load "projectile"
+  (add-to-list 'projectile-project-root-files-bottom-up "pubspec.yaml")
+  (add-to-list 'projectile-project-root-files-bottom-up "BUILD"))
+(setq lsp-auto-guess-root t)
 
 (provide 'init-dart)
