a081a994f5fb140852af0bd8b4bd345783573195
Jaanek Oja <jaanek@oja.me>
2020-08-28 09:39:15 +0300

    Update init-lsp.el

--- init-lsp.el
+++ init-lsp.el
@@ -6,14 +6,27 @@
 (require-package 'go-mode)
 (require-package 'typescript-mode)
 
-;; company autocomplete with gocode - https://github.com/mdempsky/gocode/tree/master/emacs-company
-;; (require-package 'company)
-;; (require-package 'company-go)
-;; (add-hook 'go-mode-hook 'company-mode)(add-hook 'go-mode-hook (lambda ()  (set (make-local-variable 'company-backends) '(company-go)) (company-mode)))
+(use-package hydra)
+(use-package helm)
 
+(use-package helm-lsp
+  :config
+  (defun netrom/helm-lsp-workspace-symbol-at-point ()
+    (interactive)
+    (let ((current-prefix-arg t))
+      (call-interactively #'helm-lsp-workspace-symbol)))
+
+  (defun netrom/helm-lsp-global-workspace-symbol-at-point ()
+    (interactive)
+    (let ((current-prefix-arg t))
+      (call-interactively #'helm-lsp-global-workspace-symbol))))
+
+;; https://www.mortens.dev/blog/emacs-and-the-language-server-protocol/
 (use-package lsp-mode
+  :requires hydra helm helm-lsp
   :commands (lsp lsp-deferred)
-  :hook ((c-mode                        ; clangd
+  :hook ((lsp-completion-mode           ; enable lsp completion
+          c-mode                        ; clangd
           c-or-c++-mode                 ; clangd
           java-mode                     ; eclipse-jdtls
           js-mode                       ; ts-ls (tsserver wrapper)
@@ -23,6 +36,7 @@
           web-mode
           css-mode                      ; css-ls - pure css
           scss-mode                     ; css-ls - sass
+          yaml-mode
           ) . lsp)
   :config
   (setq lsp-prefer-flymake nil) ;; Prefer using lsp-ui (flycheck) over flymake.
@@ -32,13 +46,51 @@
   (setq lsp-enable-on-type-formatting nil)
   (setq lsp-signature-auto-activate nil)
   (setq lsp-enable-folding nil)
-  (setq lsp-enable-snippet nil)  ;; if you are using completion-at-point the snippets won’t be expanded and you should either disable them by setting lsp-enable-snippet to nil or you should switch to company-lsp
+  ;; (setq lsp-enable-snippet nil) ;; if you are using completion-at-point the snippets won’t be expanded and you should either disable them by setting lsp-enable-snippet to nil or you should switch to company-lsp
   (setq lsp-enable-completion-at-point nil)
   (setq read-process-output-max (* 1024 1024)) ;; 1mb
   (setq lsp-idle-delay 0.5)
   (setq lsp-prefer-capf t)
-  (add-to-list 'lsp-language-id-configuration '(js-jsx-mode . "javascriptreact")))
+  (add-to-list 'lsp-language-id-configuration '(js-jsx-mode . "javascriptreact"))
+  ;; (setq lsp-completion-styles '(helm-flex))
+  (setq netrom--general-lsp-hydra-heads
+        '(;; Xref
+          ("d" xref-find-definitions "Definitions" :column "Xref")
+          ("D" xref-find-definitions-other-window "-> other win")
+          ("r" xref-find-references "References")
+          ("s" netrom/helm-lsp-workspace-symbol-at-point "Helm search")
+          ("S" netrom/helm-lsp-global-workspace-symbol-at-point "Helm global search")
+
+          ;; Peek
+          ("C-d" lsp-ui-peek-find-definitions "Definitions" :column "Peek")
+          ("C-r" lsp-ui-peek-find-references "References")
+          ("C-i" lsp-ui-peek-find-implementation "Implementation")
+
+          ;; LSP
+          ("p" lsp-describe-thing-at-point "Describe at point" :column "LSP")
+          ("C-a" lsp-execute-code-action "Execute code action")
+          ("R" lsp-rename "Rename")
+          ("t" lsp-goto-type-definition "Type definition")
+          ("i" lsp-goto-implementation "Implementation")
+          ("f" helm-imenu "Filter funcs/classes (Helm)")
+          ("C-c" lsp-describe-session "Describe session")
+
+          ;; Flycheck
+          ("l" lsp-ui-flycheck-list "List errs/warns/notes" :column "Flycheck"))
+
+        netrom--misc-lsp-hydra-heads
+        '(;; Misc
+          ("q" nil "Cancel" :column "Misc")
+          ("b" pop-tag-mark "Back")))
+  )
 (add-hook 'go-mode-hook #'lsp-deferred)
+;; Create general hydra.
+(eval `(defhydra netrom/lsp-hydra (:color blue :hint nil)
+         ,@(append
+            netrom--general-lsp-hydra-heads
+            netrom--misc-lsp-hydra-heads)))
+(add-hook 'lsp-mode-hook
+          (lambda () (local-set-key (kbd "C-c C-l") 'netrom/lsp-hydra/body)))
 
 ;; optional - provides fancier overlays
 (require-package 'lsp-ui)
@@ -54,18 +106,16 @@
         lsp-ui-flycheck-list-position 'right
         lsp-ui-flycheck-live-reporting t
         lsp-ui-peek-enable t
-        lsp-ui-peek-list-width 60
-        lsp-ui-peek-peek-height 25)
+        lsp-ui-peek-list-width 80
+        lsp-ui-peek-peek-height 45
+        lsp-ui-peek-fontify 'always
+        )
 
   (add-hook 'lsp-mode-hook 'lsp-ui-mode)
   )
 
 ;; goimports - https://godoc.org/golang.org/x/tools/cmd/goimports
 (setq gofmt-command "goimports")
-
-;; lsp
-
-
 
 ;; Define function to call when go-mode loads
 ;; https://johnsogg.github.io/emacs-golang
@@ -84,7 +134,7 @@
 (defun my-ts-mode-hook ()
   (setq-default typescript-indent-level 2)
   (add-hook 'before-save-hook (function lsp-format-buffer) t t)
-  (add-hook 'before-save-hook (function lsp-organize-imports) t t)
+  ;; (add-hook 'before-save-hook (function lsp-organize-imports) t t)
   )
 (add-hook 'typescript-mode-hook 'my-ts-mode-hook)
 
